fc805cde13f3daa4d57efbf65f4a7bd4fd9be438
Anbu-official <anbu.crypto.trade@gmail.com>
2025-04-23 16:45:09 +0530

    allow all origin

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const routers = require("./route");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({ origin: "https://ai-powered-ui.vercel.app", credentials: true })
-);
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
